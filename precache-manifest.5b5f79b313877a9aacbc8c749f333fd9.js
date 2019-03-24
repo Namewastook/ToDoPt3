728decb905378bee0a106e9c7b9081a5a4cac31e
TJ <philosophy.cheer@gmail.com>
2019-03-24 16:46:39 -0400

    Updates

---
+++ precache-manifest.5b5f79b313877a9aacbc8c749f333fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f665677a222640bbda8",
+    "url": "/ToDoPt3/static/js/runtime~main.8f665677.js"
+  },
+  {
+    "revision": "b6c66b4bb91b4acdf76b",
+    "url": "/ToDoPt3/static/js/main.b6c66b4b.chunk.js"
+  },
+  {
+    "revision": "60b7f4f93f415f273fa5",
+    "url": "/ToDoPt3/static/js/1.60b7f4f9.chunk.js"
+  },
+  {
+    "revision": "b6c66b4bb91b4acdf76b",
+    "url": "/ToDoPt3/static/css/main.5360201d.chunk.css"
+  },
+  {
+    "revision": "131c0ddc65771052efa5c5d8497e1952",
+    "url": "/ToDoPt3/index.html"
+  }
+];
